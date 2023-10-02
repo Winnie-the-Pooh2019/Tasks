e913c38401b0e07fb8894a5d6a8f2189556d8354
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2023-10-02 23:00:34 +0700

    fsfas

--- src/main/kotlin/com/example/tasks/controllers/TaskMvcController.kt
+++ src/main/kotlin/com/example/tasks/controllers/TaskMvcController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping(value = ["/tasks"])
+@RequestMapping(value = [""])
 class TaskMvcController(
     @Autowired
     private val taskService: TaskService
